333cbace3f96a6020293fe9bf09cd3a7df95a63b
Arunkumar Bhat <77152354+arunkumar141997@users.noreply.github.com>
2021-06-25 22:28:30 +0530

    Delete start-streaming.sh

--- bbb-live-streaming/start-streaming.sh
+++
@@ -1,8 +0,0 @@
-#!usr/bin/sh
-. ./.env
-
-sudo docker run --rm -d \
-                --expose 8080 \
-                --name "$MEETING_ID" \
-                -v /root/bbb-live-streaming/.env:/usr/src/app/.env \
-                bbb-live-streaming:v1
