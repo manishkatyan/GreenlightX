7defc3026704ba64de52a513a6dbb1598037a65b
Arunkumar <arun@higheredlab.com>
2021-08-13 11:47:21 +0530

    Improved stream quality

--- bbb-live-streaming/bbb_stream.js
+++ bbb-live-streaming/bbb_stream.js
@@ -21,10 +21,10 @@ var disp_num = Math.floor(Math.random() * (200 - 99) + 99);
 var xvfb = new Xvfb({
     displayNum: disp_num,
     silent: true,
-    xvfb_args: ["-screen", "0", "1280x800x24", "-ac", "-nolisten", "tcp", "-dpi", "96", "+extension", "RANDR"]
+    xvfb_args: ["-screen", "0", "1920x1080x24", "-ac", "-nolisten", "tcp", "-dpi", "96", "+extension", "RANDR"]
 });
-var width       = 1280;
-var height      = 800;
+var width       = 1920;
+var height      = 1080;
 var options     = {
   headless: false,
   args: [
